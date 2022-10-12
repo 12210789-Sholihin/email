569232cd576ac87dc0dd9ef61a7928cdf2911372
12210789-Sholihin <12210789@bsi.ac.id>
2022-10-12 22:46:44 +0700

    email

---
+++ app/Database/Seeds/2022-09-22-044935_Pustakawan.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Pustakawan extends Seeder
+{
+    public function run()
+    {
+        $mp = new PenggunaModel();
+        $r 
+    }
+
+
+   
+}
